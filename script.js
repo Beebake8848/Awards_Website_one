4c6237a74d7ce90a0f75542b41d91c97ccf02a46
Beebake8848 <kbibek448@gmail.com>
2023-10-08 23:06:20 +0545

    added

---
+++ script.js
@@ -0,0 +1,73 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+function circleMouseFollower(xscale = 1, yscale = 1) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const minicircle = document.querySelector("#minicircle");
+
+    if (!minicircle) {
+      console.error("Element with id 'minicircle' not found.");
+      return;
+    }
+
+    window.addEventListener("mousemove", function (event) {
+      minicircle.style.transform = `translate(${event.clientX}px, ${event.clientY}px) scale(${xscale}, ${yscale})`;
+    });
+  });
+}
+
+circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
